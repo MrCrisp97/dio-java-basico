6c5153376167113d20959950d2802ae437d0b2e6
MrCrisp <cristianbpivoto15@gmail.com>
2024-05-13 13:47:39 -0300

    ...

--- projeto-smart-tv/src/Usuario.java
+++ projeto-smart-tv/src/Usuario.java
@@ -1,7 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Canal Atual: " + smartTv.canal);
@@ -9,8 +13,10 @@ public class Usuario {
 
         smartTv.ligar();
         System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
-
-        smartTv.mudarCanal(13);
+        
+        System.out.println("Digite o canal");
+        int escolhaCanal = scanner.nextInt();
+        smartTv.mudarCanal(escolhaCanal);
         System.out.println("Canal Atual: " + smartTv.canal);
     }
 }
